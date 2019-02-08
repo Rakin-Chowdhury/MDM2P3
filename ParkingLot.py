7e0de7a9f09390b5d439a8641fd20dcc61a0ffae
Rakin Chowdhury <Rc16453@bristol.ac.uk>
2019-02-08 10:32:47 +0000

    Code as of 8 Feb 10:32

---
+++ ParkingLot.py
@@ -0,0 +1,45 @@
+#Title - Calculating optimum size of a parking lot
+#Author - Rakin Chowdhury
+#Owners - Rakin Chowdhury, Florence Townend, James Senior, Freiya Ketteridge
+#Date - 07/02/19
+
+
+# numpy and pandas pakages require external instaltion
+import numpy as np
+import math
+import pandas as pd
+
+# BlockingProb(phi, c) - Function to return blocking probailty for a M/G/c/c Queue,
+# Takes phi = (lamda(rate) / mu (1/mean)) as [int or float] and c (available parking spots) as [int only]
+# Returns Bc (Blocking probailty for given phi and mu) as [float]
+def BlockingProb(phi, c):
+    # This function will use an itterative numercial method to caluate the Blocking probablity
+    # starting from c = 0 (no parking spaces) to c = c (c parking spaces)
+
+    # initial value, at c = 0, B = 1 as there is no avliabvle spaces
+    Bn = 1
+
+    #itterate formula from c = 0 to c = c. This will return Bc (blocking ratio at c)
+    for cn in range(c):
+        Bn = (phi * Bn) / ((cn+1) + phi * Bn)
+
+    return Bn
+
+# VarryC(phi, cArray) = Function to calcuate the blokcing prob for different values of c,
+#takes phi as [float or int] and as cArray [Array[int]]
+# returns table (table of results) as [Pandas.DataFrame]
+def VarryC(phi, cArray):
+    results = [] # Array to store results
+
+    #itterate over all values of c and append result to the array "results"
+    for cn in cArray:
+        B =  BlockingProb(phi, cn)
+        results.append([cn, B])
+
+    #Adding data to pandas data frame for ease of comparison
+    table = pd.DataFrame(results,  columns = ['Parking Spaces', 'Blocking Probailty'])
+    return table
+
+
+print("For lamda = 60, and mean = 2.5 and  and varrying parking spaces [150, 155, 160]")
+print(VarryC(150, [150,155,160]))
