befa799688f3b865cb3f546d48a00320a4b03dcc
Rakin Chowdhury <Rc16453@bristol.ac.uk>
2019-02-09 12:22:00 +0000

    Code Finalised

--- ParkingLot.py
+++ ParkingLot.py
@@ -9,23 +9,26 @@ import numpy as np
 import math
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
 
 # BlockingProb(phi, c) - Function to return blocking probailty for a M/G/c/c Queue,
-# Takes phi = (lamda(rate) / mu (1/mean)) as [int or float] and c (available parking spots) as [int only]
+# Takes phi = (lamda / mu ) as [int or float] and c (available parking spots) as [int only]
 # Returns Bc (Blocking probailty for given phi and mu) as [float]
 def BlockingProb(phi, c):
     # This function will use an itterative numercial method to caluate the Blocking probablity
-    # starting from c = 0 (no parking spaces) to c = c (c parking spaces)
+    # starting from cn = 0 (no parking spaces) to cn = c (c parking spaces)
 
     # initial value, at c = 0, B = 1 as there is no avliabvle spaces
     Bn = 1
 
-    #itterate formula from c = 0 to c = c. This will return Bc (blocking ratio at c)
+    #itterate formula from cn = 0 to cn = c. This will return Bc (blocking ratio at c)
     for cn in range(c):
         Bn = (phi * Bn) / ((cn+1) + phi * Bn)
 
     return Bn
 
+
+#Used for testing
 # VarryC(phi, cArray) = Function to calcuate the blokcing prob for different values of c,
 #takes phi as [float or int] and as cArray [Array[int]]
 # returns table (table of results) as [Pandas.DataFrame]
@@ -42,7 +45,7 @@ def VarryC(phi, cArray):
     return table
 
 #Function to caluate the minum number of parking spaces need with a given phiself
-#Takes phi  as [int or Float], c0 (initial c (number of parking spots)) as [int only] and blockingThershold (probablity) as [Float]
+#Takes phi  as [int or Float] and blockingThershold (probablity) as [Float]
 #Returns c (minimum parking spots within threshold) [int], bn (Bloacking Probailty) [Float]
 def MinimumBn(phi,blockingThershold):
     bn = 1
@@ -51,20 +54,76 @@ def MinimumBn(phi,blockingThershold):
         cn = cn + 1
         bn = BlockingProb(phi, cn)
 
+
     return[cn, bn]
 
+# Function to Visually show how the optimum value is optained
+#Takes phi  as [int or Float] and blockingThershold (probablity) as [Float]
+#Returns pyplot graph
+def MinBnVisual(phi, threshold):
+    bn = 1
+    cn = 1
+    bArray = []
+    cArray = []
+    FoundOptimum = False
+    while bn > 0.0000001:
+        cn = cn + 1
+        bn = BlockingProb(phi, cn)
+        cArray.append(cn)
+        bArray.append(bn)
+        if FoundOptimum == True:
+            b = bn
+            c = cn
+            FoundOptimum = False
+        if bn > threshold:
+            FoundOptimum = True
 
-def main(phi, Bt):
-    print(MinimumBn(phi, Bt))
 
-if __name__=='__main__' and len(sys.argv) ==3:
-    main(int(sys.argv[1]), float(sys.argv[2]))
 
-else:
+
+    plt.axhline(y = threshold, linestyle='--', color = 'g')
+    plt.plot(cArray, bArray, 'r')
+    plt.plot([c],[b], 'o')
+    plt.annotate(f"({c} , {round(b,4)} )", (c,b), xytext=(c, b + 0.1), arrowprops=dict(arrowstyle="->"))
+    plt.xlabel('Parking Spaces (c)')
+    plt.ylabel('Blocking Probailty (b)')
+    plt.legend(["Minimum Threshold","Blocking Probailty for given c", "Optimum Value"])
+    plt.title("Calculating The Minimum Required Parking Spaces")
+    plt.show()
+
+    return
+
+# help Function
+def help():
     print("""
-    Use:
-    argv[1] = phi (lambda / mu)
-    argv[2] = Thershold (0.005 for project) 
+    Help:
 
-    """)
-#print(VarryC(150, [150, 155, 160]))
+    Ussage:
+    - MinBn [Phi] [Minimum Bn]
+        "Returns Maxmium c for Minimum Bn"
+    - VisBn [Phi] [Minimum Bn]
+        "Plots Bn agaisnt Cn"
+
+    Key:
+    - Lamba = Rate
+    - mu = 1 / Mean
+    - Phi = Lamba / mu
+    - Bn = Blocking Probailty
+    - c = Parking Spaces
+
+            """)
+
+
+#Main
+if __name__=='__main__':
+
+    if len(sys.argv) != 4:
+        help()
+    elif sys.argv[1] == "MinBn":
+        print(MinimumBn(float(sys.argv[2]), float(sys.argv[3])))
+    elif sys.argv[1] == "VisBn":
+        MinBnVisual(float(sys.argv[2]), float(sys.argv[3]))
+
+    else:
+        print("Incorrect Ussage")
+        help()
